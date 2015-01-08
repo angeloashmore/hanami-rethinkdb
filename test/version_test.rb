b168208c97fe7799da53e0f566151d1b62d1e806
Angelo Ashmore <angeloashmore@me.com>
2015-01-07 14:08:14 -1000

    Bump version to 0.1.1

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Lotus::Model::Adapters::Rethinkdb::VERSION do
   it 'returns current version' do
-    Lotus::Model::Adapters::Rethinkdb::VERSION.must_equal '0.1.0'
+    Lotus::Model::Adapters::Rethinkdb::VERSION.must_equal '0.1.1'
   end
 end
