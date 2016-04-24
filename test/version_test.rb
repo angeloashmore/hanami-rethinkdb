8feb6ae2feecfcd0a3c908bc48e4315a370d8ffe
Darkslategrey <dslategrey@gmail.com>
2016-04-24 05:00:52 +0200

    fix version number

--- test/version_test.rb
+++ test/version_test.rb
@@ -2,6 +2,6 @@ require 'test_helper'
 
 describe Hanami::Rethinkdb::VERSION do
   it 'returns current version' do
-    Hanami::Rethinkdb::VERSION.must_equal '0.2.2'
+    Hanami::Rethinkdb::VERSION.must_equal '0.2.3'
   end
 end
